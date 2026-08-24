2b50d733df89cf0010d556b490da6c9a1f9e7fa5
Dossiya Dakou <ddakou@asu.edu>
2026-08-24 21:07:11 +0800

    Export aurora UVKQ study functions

--- src/msr/visualization/__init__.py
+++ src/msr/visualization/__init__.py
@@ -1,5 +1,23 @@
 """Reproducible mathematical visualization primitives."""
 
+from msr.visualization.aurora_uvkq import (
+    HORIZON_SHIFT,
+    aurora_k,
+    aurora_q,
+    aurora_q_oscillatory_correction,
+    aurora_u,
+    aurora_v,
+)
 from msr.visualization.fields import annular_localization, coordinate_grid, radial_ring_rgb
 
-__all__ = ["annular_localization", "coordinate_grid", "radial_ring_rgb"]
+__all__ = [
+    "HORIZON_SHIFT",
+    "annular_localization",
+    "aurora_k",
+    "aurora_q",
+    "aurora_q_oscillatory_correction",
+    "aurora_u",
+    "aurora_v",
+    "coordinate_grid",
+    "radial_ring_rgb",
+]
