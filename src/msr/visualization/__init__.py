7917b4d62a91ba6394e8b56e3fd2559479000b27
Dossiya Dakou <ddakou@asu.edu>
2026-08-24 20:41:15 +0800

    feat: add visualization package

---
+++ src/msr/visualization/__init__.py
@@ -0,0 +1,5 @@
+"""Reproducible mathematical visualization primitives."""
+
+from msr.visualization.fields import annular_localization, coordinate_grid, radial_ring_rgb
+
+__all__ = ["annular_localization", "coordinate_grid", "radial_ring_rgb"]
