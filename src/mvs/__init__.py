b13257384fda3220257836b6e9c5f142a4df9498
Dossiya Dakou <ddakou@asu.edu>
2026-08-28 06:38:09 +0800

    feat(mvs): add package foundation

---
+++ src/mvs/__init__.py
@@ -0,0 +1,5 @@
+"""Mathematical Visual Design Studio vertical-slice package."""
+
+from .model import Capability, Constraint, SceneObject, Task, VisualIR
+
+__all__ = ["Capability", "Constraint", "SceneObject", "Task", "VisualIR"]
