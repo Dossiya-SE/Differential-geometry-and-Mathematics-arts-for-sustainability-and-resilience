d15442ebeda1b65af1eb7546ee123510e697afce
Dossiya Dakou <ddakou@asu.edu>
2026-08-28 07:09:10 +0800

    chore(mvs): export frozen vertical-slice contracts

--- src/mvs/__init__.py
+++ src/mvs/__init__.py
@@ -1,5 +1,26 @@
 """Mathematical Visual Design Studio vertical-slice package."""
 
-from .model import Capability, Constraint, SceneObject, Task, VisualIR
+from .model import (
+    Capability,
+    Constraint,
+    SceneObject,
+    Skill,
+    Task,
+    ValidationResult,
+    Validator,
+    VisualIR,
+)
+from .release_gate import ReleaseGateReport, evaluate_release_gates
 
-__all__ = ["Capability", "Constraint", "SceneObject", "Task", "VisualIR"]
+__all__ = [
+    "Capability",
+    "Constraint",
+    "ReleaseGateReport",
+    "SceneObject",
+    "Skill",
+    "Task",
+    "ValidationResult",
+    "Validator",
+    "VisualIR",
+    "evaluate_release_gates",
+]
